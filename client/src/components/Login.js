56d8a28728f45f92965feeac310a39bf6f683629
Jarin Prity <jarinprity438@gmail.com>
2025-07-16 00:51:43 +0600

    :)
    config issue with taliwind

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
             <svg className="mx-auto h-16 w-16 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
             </svg>
-            <h1 className="text-2xl font-bold text-gray-800 mt-2">METRO UNIVERSITY BANGLADESH</h1>
+            <h1 className="text-2xl font-bold text-gray-800 mt-2">METROPOLITAN UNIVERSITY BANGLADESH</h1>
             <p className="text-gray-600">Student Portal Login</p>
           </div>
           
